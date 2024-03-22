5ccbdee4683b95637ff3bef1e3a3ec7a0d487d17
Igor Oreshchenko <145362065+leonarus@users.noreply.github.com>
2024-03-22 19:49:46 +0300

    Refactoring

--- Assets/Scripts/LuckySpin/LuckySpinScreenManager.cs
+++ Assets/Scripts/LuckySpin/LuckySpinScreenManager.cs
@@ -1,57 +1,48 @@
 using Currency;
 using Managers;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 
 public class LuckySpinScreenManager : MonoBehaviour
 {
     [SerializeField] private AudioManager _audioManager;
-    [SerializeField] 
-    private AttemptsController _attemptsController;
-    [SerializeField] 
-    private AttemptsView _attemptsView;
-    [SerializeField] 
-    private CollectedPrizesController _collectedPrizesController;
-    [SerializeField] 
-    private CollectedPrizesView _collectedPrizesView;
+    [SerializeField] private AttemptsController _attemptsController;
+    [SerializeField] private AttemptsView _attemptsView;
+    [SerializeField] private CollectedPrizesController _collectedPrizesController;
+    [SerializeField] private CollectedPrizesView _collectedPrizesView;
+    [SerializeField] private CurrencyManager _coinsManager;
+    [SerializeField] private CurrencyManager _gemsManager;
+    [SerializeField] private CurrencyView _coinsView;
+    [SerializeField] private CurrencyView _gemsView;
+    [SerializeField] private LootChest _lootChest;
+    [SerializeField] private Item[] _prizes;
 
-    [SerializeField] 
-    private CurrencyManager _coins;
-    [SerializeField] 
-    private CurrencyManager _gems;
-    [SerializeField] 
-    private CurrencyView _coinsView;
-    [SerializeField] 
-    private CurrencyView _gemsView;
-    
-    [SerializeField] 
-    private LootChest _lootChest;
-    [SerializeField] 
-    private Item[] _prizes;
-
-    private void Awake()
+    private void Start()
     {
-        // При изменении количества попыток спинов - отображаем это на UI и проигрываем анимацию
-        _attemptsController.AttemptsCountChanged += OnAttemptsChanged;
-        // Подписываемся на событие подбора каждого приза изменением его количества у контроллера собранных призов
-        foreach (var prize in _prizes)
-        {
-            prize.ItemCollected += _collectedPrizesController.SetPrizeCount;
-        }
-
-        // При изменении количества призов в контроллере - изменяем это и на UI
-        _collectedPrizesController.PrizeCountChanged += _collectedPrizesView.UpdatePrizesView;
-        
-        // // При изменении количества моент/алмазов - изменяем это и на UI
-        _coins.ScoreChangedEvent += _coinsView.UpdateViewGradually;
-        _gems.ScoreChangedEvent += _gemsView.UpdateViewGradually;
-        _coins.Initialize();
-        _gems.Initialize();
-        
-        // Когда сундук собрал все призы - добавляем к счету количество собранных монет и алмазов
-        _lootChest.PrizesCollected += OnPrizesCollected;
+        SubscribeToEvents();
+        InitializeCurrency();
     }
     
+    private void SubscribeToEvents()
+    {
+        _attemptsController.AttemptsCountChanged += OnAttemptsChanged;
+        foreach (var prize in _prizes)
+        {
+            if (prize != null)
+                prize.ItemCollected += _collectedPrizesController.SetPrizeCount;
+        }
+        _collectedPrizesController.PrizeCountChanged += _collectedPrizesView.UpdatePrizesView;
+        _coinsManager.ScoreChangedEvent += _coinsView.UpdateViewGradually;
+        _gemsManager.ScoreChangedEvent += _gemsView.UpdateViewGradually;
+        _lootChest.PrizesCollected += OnPrizesCollected;
+    }
+
+    private void InitializeCurrency()
+    {
+        _coinsManager.Initialize();
+        _gemsManager.Initialize();
+    }
+
     private void OnAttemptsChanged(int attempts)
     {
         _attemptsView.UpdateAttempts(attempts);
@@ -59,21 +50,31 @@ public class LuckySpinScreenManager : MonoBehaviour
     
     private void OnPrizesCollected()
     {
-        _coins.SetScore(_collectedPrizesController.Coins);
-        _gems.SetScore(_collectedPrizesController.Gems);
+        _coinsManager.SetScore(_collectedPrizesController.Coins);
+        _gemsManager.SetScore(_collectedPrizesController.Gems);
     }
-    
-    private void OnDestroy()
+
+    private void UnsubscribeFromEvents()
     {
         _attemptsController.AttemptsCountChanged -= OnAttemptsChanged;
         foreach (var prize in _prizes)
         {
-            prize.ItemCollected -= _collectedPrizesController.SetPrizeCount;
+            if (prize != null)
+                prize.ItemCollected -= _collectedPrizesController.SetPrizeCount;
         }
-
         _collectedPrizesController.PrizeCountChanged -= _collectedPrizesView.UpdatePrizesView;
-        _coins.ScoreChangedEvent -= _coinsView.UpdateViewGradually;
-        _gems.ScoreChangedEvent -= _gemsView.UpdateViewGradually;
+        _coinsManager.ScoreChangedEvent -= _coinsView.UpdateViewGradually;
+        _gemsManager.ScoreChangedEvent -= _gemsView.UpdateViewGradually;
         _lootChest.PrizesCollected -= OnPrizesCollected;
+    }
+    
+    private void OnDestroy()
+    {
+        UnsubscribeFromEvents();
+    }
+    
+    public void OpenLobbyScreen()
+    {
+        SceneManager.LoadScene(GlobalConstants.LOBBY_SCENE);
     }
 }
